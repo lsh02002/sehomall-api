d773716891f65ae758f9068d8c01145cf3962d33
lsh02002 <lsh02002@hanmail.net>
2025-01-04 16:22:21 +0900

    fix corsConfigurationSource

--- src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
+++ src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///src/main/resources/static/images/");
     }
 }
