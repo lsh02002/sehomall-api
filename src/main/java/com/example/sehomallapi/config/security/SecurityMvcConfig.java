dbcc9a758b9d01c0ca3a77f1065e5a1d11eff874
lsh02002 <lsh02002@hanmail.net>
2025-01-04 14:46:31 +0900

    fix addResourceHandlers

--- src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
+++ src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/").setCachePeriod(0);
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
     }
 }
