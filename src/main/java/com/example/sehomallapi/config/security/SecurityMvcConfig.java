5943dc174f8288ae9b821e3b7c7061b3d2b8ed81
lsh02002 <lsh02002@hanmail.net>
2025-01-04 19:11:25 +0900

    fix addResourceHandlers

--- src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
+++ src/main/java/com/example/sehomallapi/config/security/SecurityMvcConfig.java
@@ -1,12 +1,9 @@
 package com.example.sehomallapi.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
@@ -14,7 +11,7 @@ public class SecurityMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
-
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(0);
     }
 }
