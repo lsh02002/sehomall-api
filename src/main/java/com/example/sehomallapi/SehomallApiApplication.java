6a1603fc2656cd885398b8104ead9cbd8cb0c29d
lsh02002 <lsh02002@hanmail.net>
2025-01-17 17:02:49 +0900

    add reviewCount

--- src/main/java/com/example/sehomallapi/SehomallApiApplication.java
+++ src/main/java/com/example/sehomallapi/SehomallApiApplication.java
@@ -1,9 +1,12 @@
 package com.example.sehomallapi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @EnableCaching
 @EnableJpaAuditing
@@ -14,4 +17,9 @@ public class SehomallApiApplication {
         SpringApplication.run(SehomallApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
