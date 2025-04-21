f0cc3e607b563edcd3c1e4b2908816f031366e21
lsh02002 <lsh02002@hanmail.net>
2025-04-21 15:44:48 +0900

    fix redis functions#3

--- src/main/java/com/example/sehomallapi/service/item/ItemService.java
+++ src/main/java/com/example/sehomallapi/service/item/ItemService.java
@@ -60,7 +60,7 @@ public class ItemService {
     }
 
     @Transactional
-    @Cacheable(key = "'cate'+#pageable.pageNumber", value = "item")
+    @Cacheable(key = "'cate'+#category", value = "item")
     public RestPage<ItemResponse> getAllItemsByCategory(String category, Pageable pageable) {
         return new RestPage<>(itemRepository.findByCategory(category, pageable)
                 .map(this::convertToItemResponse));
